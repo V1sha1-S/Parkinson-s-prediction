7cb39fc0de54b9ad63d0fd3d2591f6504d2b602c
V1sha1-S <76083887+V1sha1-S@users.noreply.github.com>
2023-04-28 12:30:17 +0530

    initial3

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#Before
+
 from fastapi import FastAPI, File, UploadFile
 import os
 import logging
@@ -6,26 +8,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return { "message" : "Pakinson's" }
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    # logging.info("Received request to create_upload_file")
-    uploads_dir = "uploads"
-    if not os.path.exists(uploads_dir):
-        os.makedirs(uploads_dir)
-    file_location = f"{uploads_dir}/{file.filename}"
-    with open(file_location, "wb") as file_object:
-        file_object.write(file.file.read())
-    result=predict(file_location)
-    # print("Received request to create_upload_file")
-    print(f"File name: {file.filename}")
-    return {"filename": file.filename, "result":result}
-
-
-# Allow all domains to make requests to the API
 origins = ["*"]
 
 # Configure the CORS middleware
@@ -36,3 +18,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/")
+async def root():
+    return { "message" : "Pakinson's" }
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    uploads_dir = "uploads"
+    if not os.path.exists(uploads_dir):
+        os.makedirs(uploads_dir)
+    file_location = f"{uploads_dir}/{file.filename}"
+    with open(file_location, "wb") as file_object:
+        file_object.write(file.file.read())
+    result=predict(file_location)
+    return {"filename": file.filename, "result":result}
+
+
+# Allow all domains to make requests to the API
